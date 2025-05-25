a6c2fc8a4b554ac065df7b4a539c3b0f990025ba
jim edward <jimssdward@gmail.com>
2025-05-25 06:39:43 +0300

    new file:   Assignment1.py 	renamed:    Assignment2 -> Assignment2.py 	renamed:    Excercise1 -> Excercise1.py

---
+++ Assignment1.py
@@ -0,0 +1,15 @@
+# Using regular function
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n-1)
+
+# Using lambda with reduce function
+from functools import reduce
+factorial_lambda = lambda n: reduce(lambda x, y: x * y, range(1, n + 1))
+
+# Test both methods
+number = 5
+
+print(f"Factorial of {number} using regular function:", factorial(number))
+print(f"Factorial of {number} using lambda function:", factorial_lambda(number))
